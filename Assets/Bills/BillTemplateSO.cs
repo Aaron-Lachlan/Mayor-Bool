deb52315be7e7603db8ed792027a54446bfc0c01
Dylan <1032456@student.sae.edu.au>
2026-04-16 22:16:01 +0800

    Accept and reject events plus internal stat change logic

--- Assets/Bills/BillTemplateSO.cs
+++ Assets/Bills/BillTemplateSO.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 [CreateAssetMenu(fileName = "Bill", menuName = "Bills/new bill", order = 1)]
 public class BillTemplateSO : ScriptableObject
@@ -9,4 +10,26 @@ public class BillTemplateSO : ScriptableObject
     public string ChangePeopleHappyAmount;
     public string ChangePollutionAmount;
 
+    public void Awake()
+    {
+        GameManager.current.EventBillRejected += ApplyBill;
+        GameManager.current.EventBillRejected += RejectBill;
+    }
+    public void OnDestroy()
+    {
+        GameManager.current.EventBillPassed -= ApplyBill;
+        GameManager.current.EventBillRejected -= RejectBill;
+    }
+    public void ApplyBill()
+    {
+        StatsManager statsManager = GameManager.FindObjectOfType<StatsManager>();
+        statsManager.ChangeMoney += int.Parse(ChangeMoneyAmount);
+        statsManager.ChangePeopleHappy += int.Parse(ChangePeopleHappyAmount);
+        statsManager.ChangePollution += int.Parse(ChangePollutionAmount);
+        //return to pool
+    }
+    public void RejectBill()
+    {
+        //return to pool
+    }
 }
